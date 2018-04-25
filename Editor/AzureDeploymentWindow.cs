7c3296554cbb38581d6daea2982599b1c6731168
David Ruttka <david@davidruttka.com>
2018-04-25 13:13:28 -0700

    Initial skeleton of a custom editor window

---
+++ Editor/AzureDeploymentWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class AzureDeploymentWindow : EditorWindow
+{
+	[MenuItem("ML on Azure/Train")]
+    static void OnAzureLogin()
+    {
+        EditorWindow.GetWindow(typeof(AzureDeploymentWindow), false, "Train ML on Azure", true);
+    }
+
+    // TODO: Good enough for initial POC. Not good enough to ship. 
+    // 1. Should remember the user's storage acct name once set, or let them select from a dropdown using Azure SDKs to populate from their subscription
+    // 2. This default wouldn't be globally unique at scale
+    string storageAccountName = $"unityml{DateTime.Now.ToString("yyyyMMddHHmm")}";
+
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("Train ML on Azure", EditorStyles.boldLabel);
+
+        storageAccountName = EditorGUILayout.TextField("Storage account name", storageAccountName);
+
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button(new GUIContent("Deploy")))
+        {
+            GUILayout.Label(storageAccountName);
+        }
+    }
+}
